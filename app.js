3fea86e356cb2c2f21eeff1eedc7aa4113fe3b1c
raomukesh <er.mukeshyadav@gmail.com>
2018-03-01 18:23:21 +0530

    Added Files.

---
+++ app.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        msg: 'Hello Vue!',
+        rawHtml: '<span style="color:red">This should be Red</span>',
+        isButtonDisabled: true,
+        seen: true,
+        url: 'https://google.com'
+    }
+})
